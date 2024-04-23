6f8aff87ecc16c9819436de1078320b31459069f
v8vettesbtester3 <v8vettesbtester3@gmail.com>
2024-04-23 16:12:38 -0400

    Minor formatting changes and error fix in workspace.xml.

--- Examples/Ex3.1_ProcImages/functions.py
+++ Examples/Ex3.1_ProcImages/functions.py
@@ -53,4 +53,20 @@ def contour1():
     cv2.waitKey()
     cv2.destroyAllWindows()
 
+def contour2():
+    img = cv2.pyrDown(cv2.imread("../../../images/hammer.jpg", cv2.IMREAD_UNCHANGED))
+
+    ret, thresh = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 127, 255, cv2.THRESH_BINARY)
+    contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+
+        # find bounding box coordinates
+        x, y, w, h = cv2.boundingRect(c)
+
+    cv2.drawContours(img, contours, -1, (255, 0, 0), 1)
+    cv2.imshow("contours", img)
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
--- Examples/Ex3.1_ProcImages/main.py
+++ Examples/Ex3.1_ProcImages/main.py
@@ -1,43 +1,43 @@
-'''
+"""
 Program: ExpProcImages
 
-A program to demonstrate image processing operatoins.
+A program to demonstrate image processing operations.
 
 Author: J. M. Hinckley
 Created: 2024
-'''
+"""
 
 from functions import *
+
 
 def main():
     # Create a menu to exercise the many functions in the functions module
 
-    LFuncs = [{"title":"EXIT","func":None},
-              {"title":"High Pass Filtering",    "func":highPassFiltering},
-              {"title":"Canny Filtering",            "func":cannyFiltering},
-              {"title":"Find a contour on a square",     "func":contour1},
+    LFuncs = [{"title": "EXIT", "func": None},
+              {"title": "High Pass Filtering", "func": highPassFiltering},
+              {"title": "Canny Filtering", "func": cannyFiltering},
+              {"title": "Find a contour on a square", "func": contour1},
               ]
 
-
-    while True:                      # continue until an exit is requested
-        print("\n"+"-"*15+"Action Menu"+"-"*15)    # print a menu
+    while True:  # continue until an exit is requested
+        print("\n" + "-" * 15 + "Action Menu" + "-" * 15)  # print a menu
         for i in range(len(LFuncs)):
-            print(i,LFuncs[i]["title"])
+            print(i, LFuncs[i]["title"])
         print()
 
         choice = -1
         while choice < 0 or choice >= len(LFuncs):  # get the user's choice
-            msg = "Enter your choice (0-"+str(len(LFuncs)-1)+"): "
+            msg = "Enter your choice (0-" + str(len(LFuncs) - 1) + "): "
             choice = int(input(msg))
         print()
 
         if choice != 0:
-            LFuncs[choice]["func"]()    # run the selected function
+            LFuncs[choice]["func"]()  # run the selected function
         else:
-            break                       # Exit requested, so leave
+            break  # Exit requested, so leave
 
     print("Done.")
 
+
 if __name__ == '__main__':
     main()
-

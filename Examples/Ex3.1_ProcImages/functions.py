b5ad42227a8e027982d1e0ef5bb0fac51fbe0450
v8vettesbtester3 <v8vettesbtester3@gmail.com>
2024-04-24 15:41:11 -0400

    Added code to function contour2 in Ex3.1.

--- Examples/Ex3.1_ProcImages/functions.py
+++ Examples/Ex3.1_ProcImages/functions.py
@@ -54,10 +54,35 @@ def contour1():
     cv2.destroyAllWindows()
 
 def contour2():
-    img = cv2.pyrDown(cv2.imread("../../../images/hammer.jpg", cv2.IMREAD_UNCHANGED))
+    # Read a sample image file, without changing it.  Adjust your path to match your location of the images folder.
+    # Then, down-sample the input image using pyrDown()
+    # Reference: https://docs.opencv.org/4.x/d4/d1f/tutorial_pyramids.html
+    img = cv2.imread("../../../images/hammer.jpg", cv2.IMREAD_UNCHANGED)
+    cv2.imshow("Source: Full Size", img)
+    img = cv2.pyrDown(img)
+    cv2.imshow("Source: Half Size", img)
 
-    ret, thresh = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 127, 255, cv2.THRESH_BINARY)
-    contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Convert the color image to a grayscale image, then threshold.
+    # Thresholding in THRESH_BINARY mode: if source pixel intensity is > thresh (127), then it is set to maxval (255)
+    # Return values: ret is the threshold used (127) and thresh is the thresholded image.
+    # Reference: https://docs.opencv.org/4.9.0/d7/d4d/tutorial_py_thresholding.html
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    cv2.imshow("Thresholded", thresh)
+
+    # Find the contours in a binary image
+    # Return values: contours is a list of the contours found,
+    # hier[0] is a 2D array
+    # hier[0][i] are the 4 hierarchical elements of contour i
+    # hier[0][i][0] is the index of the next contour
+    # hier[0][i][1] is the index of the previous contour
+    # hier[0][i][2] is the index of the first child contour
+    # hier[0][i][3] is the index of the parent contour
+    # if any hier[0][i][j] is negative, the corresponding contour does not exist
+    contours, hier = cv2.findContours(thresh,                   # source image
+                                      cv2.RETR_EXTERNAL,        # contour retrieval mode
+                                      cv2.CHAIN_APPROX_SIMPLE)  # contour approximation method
 
     for c in contours:
 
@@ -84,6 +109,8 @@ def contour2():
 
     cv2.drawContours(img, contours, -1, (255, 0, 0), 1)
     cv2.imshow("contours", img)
+
+    cv2.imwrite('h.jpg', img)
 
     cv2.waitKey()
     cv2.destroyAllWindows()

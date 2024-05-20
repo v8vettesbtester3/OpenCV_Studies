c3e91e2d431ea23985dced0324cecec637d8008e
v8vettesbtester3 <v8vettesbtester3@gmail.com>
2024-05-20 14:26:33 -0400

    Debugging face detection in video.

--- Examples/Ex5.1_FaceDetection/functions.py
+++ Examples/Ex5.1_FaceDetection/functions.py
@@ -50,7 +50,7 @@ def detectFaceVideo():
     face_cascade = cv2.CascadeClassifier("./cascades/haarcascade_frontalface_default.xml")
     eye_cascade = cv2.CascadeClassifier("./cascades/haarcascade_eye.xml")
 
-    cv2.namedWindow('Video Faces')
+    #cv2.namedWindow('Video Faces')
 
 
     camera = cv2.VideoCapture(0)
@@ -59,7 +59,7 @@ def detectFaceVideo():
         success, frame = camera.read()
 
         if success:
-            gray = cv2.cvtColor(frame, cv2.CLR_BGR2GRAY)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(120,120))
 
@@ -67,11 +67,11 @@ def detectFaceVideo():
                 cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
 
                 roi_gray = gray[y:y+h, x:x+w]
-                eyes = eye_cascade(roi_gray, 1.03, 5, minSize = (40, 40))
+                eyes = eye_cascade.detectMultiScale(roi_gray, 1.03, 5, minSize = (40, 40))
 
                 for (ex, ey, ew, eh) in eyes:
                     cv2.rectangle(frame, (x+ex,y+ey), (x+ex+ew, y+ey+eh), (0,255,0), 2)
 
-                cv2.imshow('Video Faces', frame)
+            cv2.imshow('Video Faces', frame)
 
     cv2.destroyAllWindows()

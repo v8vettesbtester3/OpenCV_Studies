54c4fc6f21b0a9349dd89e213a2c70c83422163f
v8vettesbtester3 <v8vettesbtester3@gmail.com>
2024-05-24 15:19:57 -0400

    Added Fisher and LBPH face recognition methods.

--- Examples/Ex5.1_FaceDetection/functions.py
+++ Examples/Ex5.1_FaceDetection/functions.py
@@ -189,37 +189,67 @@ def recognizeFaces():
     training_image_size = (200, 200)
     names, training_images, training_labels = read_images(path_to_training_images, training_image_size)
 
-    model = cv2.face.EigenFaceRecognizer.create()
-    model.train(training_images, training_labels)
 
-    # Set the Haar cascade that will be used to detect faces
-    face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
+    # Loop, trying different facial recognition methods
+    choice = -1
+    while choice != 0:
 
-    camera = cv2.VideoCapture(0)    # connect to the camera
+        # Print menu
+        while choice < 0 or choice > 3:
+            print('Enter choice of facial recognition method:')
+            print('1: Use EigenFaces method')
+            print('2: Use FisherFaces method')
+            print('3: Use Local Binary Pattern Histogram method')
+            print('0: quit')
+            schoice = input('')
+            choice = int(schoice)
 
-    while cv2.waitKey(1) == -1:
-        success, frame = camera.read()
-        if success:
-            faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(120,120))
+        if choice == 1:
+            model = cv2.face.EigenFaceRecognizer.create()
 
-            for (x,y, w, h) in faces:  # Loop over the faces found in the frame
-                cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)   # Draw a rectangle around the face
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                roi_gray = gray[y:y+h, x:x+w]   # isolate further analysis to just a face
+        elif choice == 2:
+            model = cv2.face.FisherFaceRecognizer.create()
 
-                if gray is None:
-                    # The file cannot be loaded as an image.
-                    # Skip it.
-                    continue
+        elif choice == 3:
+            model = cv2.face.LBPHFaceRecognizer.create()
 
-                roi_gray = cv2.resize(roi_gray, training_image_size)
-                label, confidence = model.predict(roi_gray)
+        else:
+            break
 
-                text = '%s, confidence=%.2f' % (names[label], confidence)
 
-                cv2.putText(frame, text, (x, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255,0), 2)
 
-            cv2.imshow('Face Recognition', frame)
+        model.train(training_images, training_labels)
 
-    cv2.destroyAllWindows()     # after <esc> key pressed, close all windows.
+        # Set the Haar cascade that will be used to detect faces
+        face_cascade = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
+
+        camera = cv2.VideoCapture(0)    # connect to the camera
+
+        while cv2.waitKey(1) == -1:
+            success, frame = camera.read()
+            if success:
+                faces = face_cascade.detectMultiScale(frame, 1.3, 5, minSize=(120,120))
+
+                for (x,y, w, h) in faces:  # Loop over the faces found in the frame
+                    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)   # Draw a rectangle around the face
+                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    roi_gray = gray[y:y+h, x:x+w]   # isolate further analysis to just a face
+
+                    if gray is None:
+                        # The file cannot be loaded as an image.
+                        # Skip it.
+                        continue
+
+                    roi_gray = cv2.resize(roi_gray, training_image_size)
+                    label, confidence = model.predict(roi_gray)
+
+                    text = '%s, confidence=%.2f' % (names[label], confidence)
+
+                    cv2.putText(frame, text, (x, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255,0), 2)
+
+                cv2.imshow('Face Recognition', frame)
+
+        cv2.destroyAllWindows()     # after <esc> key pressed, close all windows.
+        choice = -1
+
 

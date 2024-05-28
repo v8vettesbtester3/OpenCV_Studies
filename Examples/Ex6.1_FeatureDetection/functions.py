68173d8409383e3cedb34eec9b98d6ebd5f4ad28
v8vettesbtester3 <v8vettesbtester3@gmail.com>
2024-05-28 15:33:31 -0400

    Added a trackbar to Ex6.1

--- Examples/Ex6.1_FeatureDetection/functions.py
+++ Examples/Ex6.1_FeatureDetection/functions.py
@@ -3,30 +3,39 @@ import numpy
 import os
 from matplotlib import pyplot as plt
 
-def detectHarrisCorners():
-    # detect corners in a static image using the Harris method
+def update(sliderValue = 3):
+    global gray
+    global img
+
+    val = cv2.getTrackbarPos('val','Harris Corners')
+    if val % 2 == 0:
+        val += 1
+    if val > 31:
+        val = 31
+    if val < 3:
+        val = 3
 
     img = cv2.imread('../../../images/5_of_diamonds.png') # read the static image file
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    # make a grayscale image
 
-    dst = cv2.cornerHarris(gray, 2, 23, 0.04)
-
-
-    # Taking a matrix of size 5 as the kernel
-    #kernel = numpy.ones((5, 5), numpy.uint8)
-
-    # The first parameter is the original image,
-    # kernel is the matrix with which image is
-    # convolved and third parameter is the number
-    # of iterations, which will determine how much
-    # you want to erode/dilate a given image.
-    #dst = cv2.dilate(dst, kernel, iterations = 1)
-
+    dst = cv2.cornerHarris(gray, 2, val, 0.04)
 
     img[dst > 0.01 * dst.max()] = [0, 255, 0]
 
-    cv2.imshow("Harris corners", img)
+    cv2.imshow("Harris Corners", img)
+
+
+
+def detectHarrisCorners():
+    # detect corners in a static image using the Harris method
+    global gray
+    global img
+
+    cv2.namedWindow("Harris Corners")
+    cv2.createTrackbar('val', 'Harris Corners', 3, 31, update)
+
+    update()
 
     cv2.waitKey()
 
